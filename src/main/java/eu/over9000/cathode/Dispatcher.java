bc3129a8d84b24a907e58185a736260291c660d9
Jan Strauß <jan@over9000.eu>
2016-05-06 04:49:11 +0200

    fix image retrieval method

--- src/main/java/eu/over9000/cathode/Dispatcher.java
+++ src/main/java/eu/over9000/cathode/Dispatcher.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2014-2016 s1mpl3x <jan[at]over9000.eu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package eu.over9000.cathode;
 
 import com.google.gson.FieldNamingPolicy;
@@ -114,13 +136,14 @@ public class Dispatcher {
 
 	public <ImageFormat> Result<ImageFormat> retrieveImage(final String url, final Function<InputStream, ImageFormat> converter) {
 		try {
-			System.out.println(url);
 			final RequestBuilder requestBuilder = RequestBuilder.get(url);
 			final HttpUriRequest request = requestBuilder.build();
 			final HttpResponse httpResponse = HTTP_CLIENT.execute(request);
 
 			final InputStream inputStream = httpResponse.getEntity().getContent();
 			final ImageFormat image = converter.apply(inputStream);
+			inputStream.close();
+
 			return new Result<>(image);
 
 		} catch (final Exception e) {
