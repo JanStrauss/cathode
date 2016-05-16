4c33f8cbf8e82e54f3a61747aca33c7747afc057
Jan Strauß <jan@over9000.eu>
2016-05-16 04:51:58 +0200

    fix possible npe in putFollows method when passed a null option added getOrException method to Result class

--- src/main/java/eu/over9000/cathode/Result.java
+++ src/main/java/eu/over9000/cathode/Result.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2014-2016 s1mpl3x <jan[at]over9000.eu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package eu.over9000.cathode;
 
 import java.util.Optional;
@@ -5,12 +27,9 @@ import java.util.function.Consumer;
 
 public class Result<Type> {
 
-	private enum State {OK, ERROR}
-
 	private final State state;
 	private final Type value;
 	private final Throwable error;
-
 	public Result(final Type value) {
 		this.value = value;
 		this.state = State.OK;
@@ -52,6 +71,13 @@ public class Result<Type> {
 		return Optional.ofNullable(value);
 	}
 
+	public Type getOrException() throws Exception {
+		if (isOk()) {
+			return value;
+		}
+		throw new Exception(error);
+	}
+
 	public Type getResultRaw() {
 		return value;
 	}
@@ -59,4 +85,7 @@ public class Result<Type> {
 	public Throwable getErrorRaw() {
 		return error;
 	}
+
+	private enum State {OK, ERROR}
+
 }
--- src/main/java/eu/over9000/cathode/endpoints/implementations/UsersImpl.java
+++ src/main/java/eu/over9000/cathode/endpoints/implementations/UsersImpl.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2014-2016 s1mpl3x <jan[at]over9000.eu>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package eu.over9000.cathode.endpoints.implementations;
 
 import eu.over9000.cathode.Dispatcher;
@@ -53,7 +75,7 @@ public class UsersImpl extends AbstractEndpoint implements Users {
 
 	@Override
 	public Result<Follow> putFollows(final String userName, final String targetName, final PutFollowsOptions options) {
-		return dispatcher.performPut(Follow.class, Users.PATH + "/" + userName + "/follows/channels/" + targetName, new StringEntity(options.encode(), ContentType.APPLICATION_FORM_URLENCODED));
+		return dispatcher.performPut(Follow.class, Users.PATH + "/" + userName + "/follows/channels/" + targetName, options == null ? null : new StringEntity(options.encode(), ContentType.APPLICATION_FORM_URLENCODED));
 	}
 
 	@Override
